5d6f75c9ca3cde4b72e34475a3f72e046fab22e8
Myo Min Htoo <mmhtoo.dev@gmail.com>
2025-04-14 08:16:32 +0630

    feat: validation middleware res tpe

--- middlewares/check_validation_middleware.go
+++ middlewares/check_validation_middleware.go
@@ -5,16 +5,17 @@ import (
 	"reflect"
 
 	"github.com/gin-gonic/gin"
+	"github.com/mmhtoo/go-logger-api/helpers"
 )
 
 func CheckValidationMiddleware(schema any) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		obj := reflect.New(reflect.TypeOf(schema)).Interface()
 		if err := c.ShouldBindJSON(obj); err != nil {
-			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-				"error": err.Error(),
-				"message": "Validation failed!",
-			})
+			c.AbortWithStatusJSON(
+				http.StatusBadRequest, 
+				helpers.NewAPIErrorResponse(err.Error(), "Validation failed!"),
+			)
 			return
 		}
 		c.Set("payload", obj)
